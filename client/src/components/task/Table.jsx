fcd7268f148720d97a3919fbf2ce4c8f9890de51
Alamin11 <alamincsecu@gmail.com>
2025-01-05 18:07:03 +0600

    fixed task list system and employee access

--- client/src/components/task/Table.jsx
+++ client/src/components/task/Table.jsx
@@ -58,18 +58,18 @@ const Table = ({ tasks }) => {
   const TableHeader = () => (
     <thead className="w-full border-b border-gray-300">
       <tr className="w-full text-black  text-left">
-        <th className="py-2">Task Title</th>
-        <th className="py-2">Priority</th>
-        <th className="py-2 line-clamp-1">Created At</th>
-        <th className="py-2">Assets</th>
-        <th className="py-2">Team</th>
+        <th className="p-2">Task Title</th>
+        <th className="p-2">Priority</th>
+        <th className="p-2 line-clamp-1">Created</th>
+        <th className="p-2">Assets</th>
+        <th className="p-2">Team</th>
       </tr>
     </thead>
   );
 
   const TableRow = ({ task }) => (
     <tr className="border-b border-gray-200 text-gray-600 hover:bg-gray-300/10">
-      <td className="py-2">
+      <td className="p-2">
         <div className="flex items-center gap-2">
           <div
             className={clsx("w-4 h-4 rounded-full", TASK_TYPE[task.stage])}
@@ -80,25 +80,30 @@ const Table = ({ tasks }) => {
         </div>
       </td>
 
-      <td className="py-2">
+      <td className="p-2">
         <div className={"flex gap-1 items-center"}>
-          <span className={clsx("text-lg", PRIOTITYSTYELS[task?.priority])}>
+          <span
+            className={clsx(
+              "text-lg lg:text-sm ",
+              PRIOTITYSTYELS[task?.priority]
+            )}
+          >
             {ICONS[task?.priority]}
           </span>
-          <span className="capitalize line-clamp-1">
+          <span className="text-lg lg:text-sm capitalize line-clamp-1">
             {task?.priority} Priority
           </span>
         </div>
       </td>
 
-      <td className="py-2">
+      <td className="p-2">
         <span className="text-sm text-gray-600">
           {formatDate(new Date(task?.date))}
         </span>
       </td>
 
-      <td className="py-2">
-        <div className="flex items-center gap-3">
+      <td className="p-2">
+        <div className="flex items-center gap-1">
           <div className="flex gap-1 items-center text-sm text-gray-600">
             <BiMessageAltDetail />
             <span>{task?.activities?.length}</span>
@@ -114,7 +119,7 @@ const Table = ({ tasks }) => {
         </div>
       </td>
 
-      <td className="py-2">
+      <td className="p-2">
         <div className="flex">
           {task?.team?.map((m, index) => (
             <div
@@ -130,7 +135,7 @@ const Table = ({ tasks }) => {
         </div>
       </td>
 
-      <td className="py-2 flex gap-2 md:gap-4 justify-end">
+      <td className="p-2 flex gap-2 md:gap-4 justify-end">
         <Button
           className="text-blue-600 hover:text-blue-500 sm:px-0 text-sm md:text-base"
           label="Edit"
