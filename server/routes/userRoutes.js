ccc71a441847d8c70a28fb1fb600ef77ce19401b
Alamin11 <alamincsecu@gmail.com>
2025-01-07 16:45:02 +0600

    debugging git problem

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -14,6 +14,7 @@ import {
 } from "../controllers/userController.js";
 
 const router = express.Router();
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
