4c69113100696dd94b90284802ed922a3996a277
Alamin11 <alamincsecu@gmail.com>
2025-03-03 11:36:02 +0600

    fixing deployment issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://task-manager-beta-silk.vercel.app/",
-        // target: "http://localhost:8800",
+        // target: "https://task-manager-beta-silk.vercel.app/",
+        target: "http://localhost:8800",
 
         changeOrigin: true,
       },
