b74ed6b1888a4063dd0cd2d833eaeabe2ff782b6
Alamin11 <alamincsecu@gmail.com>
2025-03-06 16:06:20 +0600

    Login page responsiveness okay

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -44,16 +44,16 @@ const Login = () => {
   }, [user, navigate]);
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
-      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center px-20">
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6] py-10">
+      <div className="w-full md:w-auto flex gap-0 md:gap-16 flex-col md:flex-row items-center justify-center md:px-20">
         {/* left side */}
         <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
           <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
             <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600">
               Manage all your task in one place!
             </span>
-            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
-              <span>Cloud-Based</span>
+            <p className="flex flex-col gap-0 md:gap-2 text-4xl lg:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
+              <span>AirSpace</span>
               <span>Task Manager</span>
             </p>
 
@@ -67,13 +67,11 @@ const Login = () => {
         <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
           <form
             onSubmit={handleSubmit(submitHandler)}
-            className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14"
+            className="form-container w-full md:w-[320px] lg:w-[400px] flex flex-col gap-y-8 bg-white px-10 py-14"
           >
-            <div className="">
-              <p className="text-blue-600 text-3xl font-bold text-center">
-                Welcome back!
-              </p>
-              <p className="text-center text-base text-gray-700 ">
+            <div className="text-center">
+              <p className="text-blue-600 text-3xl font-bold ">Welcome back!</p>
+              <p className=" text-base text-gray-700 ">
                 Keep all your credential safe.
               </p>
             </div>
